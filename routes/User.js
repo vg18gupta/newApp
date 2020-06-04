b8dcb603b4c052e28e478d968fb40f95ad4a03ac
vishal gupta <vg.vishal.gupta.10@gmail.com>
2020-06-04 15:58:09 +0530

    changed

--- routes/User.js
+++ routes/User.js
@@ -2,7 +2,7 @@ const express = require('express')
 const users = express.Router()
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 const checkAuth=require('./checkAuth')
 
